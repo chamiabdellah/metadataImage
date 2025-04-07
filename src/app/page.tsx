ec9755392d787e4fae5abfd9616c7273dab01f5b
AbdellahChami <abdellahchami@imedia24.de>
2025-04-07 09:47:29 +0100

    test openGraph type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,98 +4,6 @@ import Head from "next/head";
 export default function Home() {
   return (
       <>
-       <Head>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-xgplayer.caf0854def3014a00596.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-lottie.cb3fdc850e41a2c46f7a.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-xg-helper.1731ab49b2fefb076dda.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-596046b7.766b7d79a23b76b5a119.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-f9d79dd4.4a62af3d06de2fcec9e7.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-e7d1e88f.efb6cbd74940a0fe58cb.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-897bfa5e.6f2317881ca0a1345416.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-87e0bff3.2726b3f243dca54529c2.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-5a799721.05845ed2c552fe80e7da.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-cdf20b09.62e1e0e83ed6b8d334bb.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-82495f79.f7457422c051f9c17a8c.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-d98dc4ed.dc8e67b22338a075446a.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-9f81b684.0da89852c90b6f0e159b.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-58cded4b.c72d3e2df3fba37622be.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-ed7c8e09.ff2b57c76c12c7efcb56.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-2d6263f2.cdb8e1a709fba6b19cb9.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-032a595f.ce1c9b2d65ba45c1ca4a.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-b8ad27b0.63576c173ec3260f2d19.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-6801073e.f6d8c2c1b9e328206121.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-ca20f4a9.a5d7896a36cfc628a516.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-754771ea.ccccd73f3932d439a2a0.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-cbf8c0c2.1e01c0f475547e4d7365.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-5513673c.c7fc84dc8984a4787e3c.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-d10553a4.9a0b34deac83278d9264.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-e1a0c311.7f40237562288668ea2b.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/5890.b2b35447acf313a0a892.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/1211.728a33c991e37990b81a.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/2301.d4b1cad71a8d8dcce88a.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/2110.a33db5409565f8a4b8c6.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/4955.aba72d0611cbec6ce15c.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/5268.54a59c52abc8f50ff2ad.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/362.32d3538d03723f5dd632.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/1386.3e632d2200f414882771.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/2590.4280430df8b8c1b01536.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/7897.8ffa931eebd61b3fb3b0.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/2235.4b7c0ac079ebfaf86e04.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/2791.d877095e61beb7a96034.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/1441.c996ff79670f10c45238.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/3328.151b0acd52d6580c794b.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/4343.7b4aa7266d854eae0ac6.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/2423.f11440993e1d08f9b6b3.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/300.b4685b04142299291a7a.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/2466.5159a7315353a5743324.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/1018.f017a50135814233983f.js"/>
-         <link data-chunk="video" rel="preload" as="script"
-               href="https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/video.1902ce651024200e0383.js"/>
-       </Head>
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
@@ -172,7 +80,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples _ 3
+          Examples _ 4
           <video
               src={videoData.videoUrl}
               poster={videoData.thumbnailUrl}
@@ -236,12 +144,13 @@ export async function generateMetadata() {
       ],
     },
     other: {
-      'link': [
+      link: [
         {
           rel: 'preload',
           as: 'script',
-          href: 'https://sf16-website-login.neutral.ttwstatic.com/obj/tiktok_web_login_static/tiktok/webapp/main/react-v17/webapp-desktop/npm-async-87e0bff3.2726b3f243dca54529c2.js',
-          'data-chunk': 'video'
+          dataChunk : 'video',
+          'data-chunk': 'video',
+          href: 'https://www.hse.de/dpl/files/streamer/streamer/2a8bde0d-594b-439c-a2a3-04426a933407/video-posts-processed/22b8f503-bd36-4526-8b22-1778cc3a7739/1080/2025_04_03_20_11_13_04f3a103b7654e99962d5170e620dbd4_preview.mp4#t=0.001',
         }
       ]
     },
