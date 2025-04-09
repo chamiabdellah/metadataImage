cb73a160df08c1b398a37ae2a245be40a38b1ec9
AbdellahChami <abdellahchami@imedia24.de>
2025-04-09 17:02:17 +0100

    test openGraph type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples _ 31
+          Examples _ 32
         </a>
         <video
             src={'https://metadata-image.vercel.app/prev.mp4'}
