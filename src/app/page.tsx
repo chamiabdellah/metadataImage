acd6d9fc138395e575f1aa94fbfb56eba8115fed
AbdellahChami <abdellahchami@imedia24.de>
2025-04-09 13:38:06 +0100

    test openGraph type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -100,7 +100,7 @@ const videoData = {
   title: "HSE Video Preview",
   description: "This test page should shows a preview of the video",
   videoUrl: "https://www.hse.de/dpl/files/streamer/streamer/2a8bde0d-594b-439c-a2a3-04426a933407/video-posts-processed/22b8f503-bd36-4526-8b22-1778cc3a7739/1080/2025_04_03_20_11_13_04f3a103b7654e99962d5170e620dbd4_preview.mp4",
-  thumbnailUrl: "https://th.bing.com/th/id/OIP.escF_r0xxbHdnM153PwyZAHaE7?rs=1&pid=ImgDetMain", // Replace with actual thumbnail
+  thumbnailUrl: "https://www.hse.de/dpl/files/streamer/streamer/c6a79dce-e2a7-458a-a849-6f1d9ccb7cc1/show/8620cbfc-61f8-4680-a133-90f251bb8fba/preview-Dz5S.jpeg?impolicy=social-commerce-policy&imwidth=740", // Replace with actual thumbnail
   duration: "5:27",
   publishedAt: "2025-04-03",
   width: 1920,
